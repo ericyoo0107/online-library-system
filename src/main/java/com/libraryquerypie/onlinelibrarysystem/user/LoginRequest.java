f9b0e58ca7dde6ba7fa08340d99968ca63f1ec17
JunHyuk Yu <ericyoo0107@gmail.com>
2025-02-12 00:24:23 +0900

    feat: DTO에 validation 추가

--- src/main/java/com/libraryquerypie/onlinelibrarysystem/user/LoginRequest.java
+++ src/main/java/com/libraryquerypie/onlinelibrarysystem/user/LoginRequest.java
@@ -1,11 +1,14 @@
 package com.libraryquerypie.onlinelibrarysystem.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "이메일 입력은 필수입니다.")
     private String emailId;
+    @NotBlank(message = "비밀번호 입력은 필수입니다.")
     private String password;
 }
--- src/main/java/com/libraryquerypie/onlinelibrarysystem/user/SignupRequest.java
+++ src/main/java/com/libraryquerypie/onlinelibrarysystem/user/SignupRequest.java
@@ -2,14 +2,18 @@ package com.libraryquerypie.onlinelibrarysystem.user;
 
 import com.libraryquerypie.onlinelibrarysystem.entity.User;
 import com.libraryquerypie.onlinelibrarysystem.enums.Role;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class SignupRequest {
+    @NotBlank(message = "이메일 입력은 필수입니다.")
     private String email;
+    @NotBlank(message = "비밀번호 입력은 필수입니다.")
     private String password;
+    @NotBlank(message = "ADMIN 또는 USER 중 하나를 선택해주세요.")
     private Role role;
 
     public static User toEntity(SignupRequest request, String encodedPassword) {
