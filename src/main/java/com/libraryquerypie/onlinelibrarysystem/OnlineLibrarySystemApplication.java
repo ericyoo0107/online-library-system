dc889dd44acaff6a1558beb4305ab9cbf9957189
JunHyuk Yu <ericyoo0107@gmail.com>
2025-02-11 15:39:18 +0900

    Initial Commit

---
+++ src/main/java/com/libraryquerypie/onlinelibrarysystem/OnlineLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.libraryquerypie.onlinelibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibrarySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/libraryquerypie/onlinelibrarysystem/OnlineLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryquerypie.onlinelibrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
