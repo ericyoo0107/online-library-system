8a6ffb7dbaf92316848618efa345c55370cfa4a2
JunHyuk Yu <ericyoo0107@gmail.com>
2025-02-13 18:34:16 +0900

    feat: 로그인/회원가입 기능에 swagger 명세 추가

--- src/main/java/com/libraryquerypie/onlinelibrarysystem/user/UserController.java
+++ src/main/java/com/libraryquerypie/onlinelibrarysystem/user/UserController.java
@@ -2,8 +2,14 @@ package com.libraryquerypie.onlinelibrarysystem.user;
 
 import com.libraryquerypie.onlinelibrarysystem.user.dto.request.LoginRequest;
 import com.libraryquerypie.onlinelibrarysystem.user.dto.request.SignupRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,17 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Tag(name = "User API", description = "User 관련 API")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "아이디, 비밀번호로 로그인을 수행하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공 후 Jwt 토큰을 반환",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)),
+            @ApiResponse(responseCode = "400", description = "회원가입 되지 않은 아이디, 비밀번호 입력",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
         String token = userService.login(loginRequest);
         return ResponseEntity.ok(token);
     }
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "회원가입을 수행하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 후 Jwt 토큰을 반환",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)),
+            @ApiResponse(responseCode = "400", description = "이미 가입되어 있는 E-MAIL인 경우",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest signupRequest) {
         String token = userService.signup(signupRequest);
         return ResponseEntity.ok(token);
