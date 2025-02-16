242c8d3c83b8cde87280f834ad17fbc93a4b9ff6
JunHyuk Yu <ericyoo0107@gmail.com>
2025-02-16 18:11:13 +0900

    docs: 동일한 책을 여러 유저가 동시에 대출 시도할 때, 비관적 락으로 동시성 문제 해결 내용 추가

--- src/main/java/com/libraryquerypie/onlinelibrarysystem/borrow/BorrowRepository.java
+++ src/main/java/com/libraryquerypie/onlinelibrarysystem/borrow/BorrowRepository.java
@@ -2,13 +2,16 @@ package com.libraryquerypie.onlinelibrarysystem.borrow;
 
 import com.libraryquerypie.onlinelibrarysystem.entity.Book;
 import com.libraryquerypie.onlinelibrarysystem.entity.Borrow;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BorrowRepository extends JpaRepository<Borrow, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select borrow from Borrow borrow where borrow.book.id = :bookId and borrow.borrowStatus = 'BORROW'")
     List<Borrow> findBorrowByBookId(@Param("bookId") Long bookId);
 
